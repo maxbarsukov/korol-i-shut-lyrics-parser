b84999adeec088a6c6378f55d54c5d119ac2fc12
maxbarsukov <maximbarsukov@bk.ru>
2021-08-02 16:50:37 +0300

    Song class added

---
+++ src/song.rb
@@ -0,0 +1,25 @@
+require 'nokogiri'
+require 'open-uri'
+
+class Song
+  attr_reader :text, :title
+
+  def initialize(url)
+    @url = url
+  end
+
+  def parse!
+    doc = Nokogiri::HTML(URI.open(@url))
+    @title = doc.css('li.active')[0].text
+    @text = doc.css('div.abp')[0]
+               .inner_html
+               .gsub('<br>', "\n")
+               .gsub('<br/>', "\n")
+               .gsub('<br />', "\n")
+  end
+
+  def parse
+    parse!
+    [@title, @text]
+  end
+end
