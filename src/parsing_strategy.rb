39166f20eb8fa2abd85d74a8b0a1cc845463869c
maxbarsukov <maximbarsukov@bk.ru>
2021-08-03 11:46:26 +0300

    Parsing strategy updated

--- src/parsing_strategy.rb
+++ src/parsing_strategy.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 class ParsingStrategy
+  attr_accessor :options
+
   def initialize(options)
     @options = options
   end
 
-  def parse(links)
+  def parse!(links)
     raise NotImplementedError,
           "#{self.class} has not implemented method '#{__method__}'"
   end
