16283d43409742083ee874b11764b302f277ddea
maxbarsukov <maximbarsukov@bk.ru>
2021-08-03 13:12:52 +0300

    Url key added

--- src/main.rb
+++ src/main.rb
@@ -22,6 +22,12 @@ HELP = <<ENDHELP
                         [Enabled by default]
    -s, --separator      Defines how song lyrics are separated.
                         [Works with -a / --allsongs only]
+   -u, --url            URL to parse.
+                        Works with site http://slushat-tekst-pesni.ru/ only!
+                        Also, you can change the artist to another one and get
+                        the lyrics of his songs
+                        Be careful, changing to another site will
+                        most likely lead to a breakdown of the application
    -d, --debug          Displays messages about the progress of the program.
                         [Disabled by default]
 ENDHELP
@@ -35,7 +41,9 @@ ARGS = Hash[ARGV.flat_map { |s| s.scan(/--?([^=\s]+)(?:=(\S+))?/) }]
 OPTIONS = { output_file: '_all_songs.txt',
             output_folder: 'output',
             with_title: true,
-            separator: "\n\n\n\n\n"}
+            separator: "\n\n\n\n\n",
+            url: 'http://slushat-tekst-pesni.ru/korol-i-shut/'
+}
 
 ARGS.each do |key, value|
   case key
@@ -47,6 +55,7 @@ ARGS.each do |key, value|
   when 'f', 'output_folder'  then OPTIONS[:output_folder]  = value
   when 's', 'separator'      then OPTIONS[:separator]      = value
   when 'd', 'debug'          then OPTIONS[:debug]          = true
+  when 'u', 'url'            then OPTIONS[:url]            = value
   end
 end
 
@@ -62,7 +71,6 @@ parsing_strategy = if OPTIONS[:by_song]
                      AllSongsStrategy.new(OPTIONS)
                    end
 
-URL = 'http://slushat-tekst-pesni.ru/korol-i-shut/'
-parser = Parser.new(URL, parsing_strategy)
+parser = Parser.new(OPTIONS[:url], parsing_strategy)
 parser.collect_links
 parser.parse_links!
