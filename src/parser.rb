d638ac3098b66cb16c88f53a2424cc8bdb735291
maxbarsukov <maximbarsukov@bk.ru>
2021-08-03 12:36:11 +0300

    Songs links accessor added

--- src/parser.rb
+++ src/parser.rb
@@ -5,24 +5,24 @@ require 'open-uri'
 require 'uri'
 
 class Parser
-  attr_accessor :parsing_strategy
+  attr_accessor :parsing_strategy, :songs_links
 
   def initialize(url, parsing_strategy)
     @url = url
     @parsing_strategy = parsing_strategy
-    @song_links = []
+    @songs_links = []
 
     uri = URI.parse(url)
     @base_url = "#{uri.scheme}://#{uri.host}"
   end
 
   def parse_links!
-    parsing_strategy.parse!(@song_links)
+    parsing_strategy.parse!(@songs_links)
   end
 
   def collect_links
     doc = Nokogiri::HTML(URI.open(@url))
-    @song_links = doc
+    @songs_links = doc
                   .xpath('//div[@class="col-sm-6"]/ul[@class="nolist"]/li/a/@href')
                   .map(&:value)
                   .map { |link| @base_url + link }
